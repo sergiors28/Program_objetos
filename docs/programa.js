a33c5cfd63b51a4bfa7637a490f34f7e6434e2f2
Jorge <lizpe0102@gmail.com>
2025-08-04 21:43:41 -0500

    v 02

---
+++ docs/programa.js
@@ -0,0 +1,5 @@
+var map = L.map('map').setView([4.628128297027276, -74.06598057680526], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
